88f760193eaf0fb1d6a9c3c56c59e9a2fae8afa0
Trifebriansah <trifebriansah321@gmail.com>
2026-09-14 01:11:48 +0700

    Ubah arsitektur ke HTTP Polling yang anti-blokir

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,24 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ServoController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-use App\Http\Controllers\Api\ServoController;
+Route::get('/servos/status', [ServoController::class, 'status']);
+Route::post('/servos/wifi', [ServoController::class, 'updateWifi']);
+Route::post('/servos/{id}/angle', [ServoController::class, 'setAngle']);
+Route::post('/servo-rules', [ServoController::class, 'saveRule']);
 
-Route::get('/servo/status', [ServoController::class, 'status']);
-Route::post('/servo/{id}/set', [ServoController::class, 'setAngle']);
-Route::post('/servo/rules', [ServoController::class, 'saveRule']);
+// Endpoint untuk menerima gambar dari ESP32
+Route::post('/camera/upload', function (Request $request) {
+    $image = $request->getContent();
+    if ($image) {
+        // Simpan langsung ke folder public agar bisa diakses browser
+        file_put_contents(public_path('stream.jpg'), $image);
+    }
+    return response()->json(['status' => 'success']);
+});
 Route::post('/wifi/config', [ServoController::class, 'updateWifi']);
